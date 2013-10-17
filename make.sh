3f609f1d72e4a980519fb1745ed479dad1a4e2aa
Phil Elson <pelson.pub@gmail.com>
2013-10-17 07:42:20 +0100

    Added auto generated exercise files

--- make.sh
+++ make.sh
@@ -9,6 +9,10 @@ do
 done
 
 
+for name in "numpy_exercise_1.ipynb" "numpy_exercise_2.ipynb" "matplotlib_exercise_2.ipynb" "matplotlib_exercise_3.ipynb" "cartopy_exercise_1.ipynb" "iris_exercise_1.ipynb" "iris_exercise_2.ipynb" "iris_exercise_3.ipynb" "iris_exercise_4.ipynb" "iris_exercise_5.ipynb" "iris_exercise_6.ipynb"
+do
+    python ../strip_code_from_notebook.py ../course_content/solutions/${name} ${name}
+done
 #ipython nbconvert --to slides 05_NumpyIntro.ipynb
 #ipython nbconvert 05_NumpyIntro.ipynb --to html
 
