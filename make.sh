5f02cad6669d8dfa1d817307bad2736253f50b15
pelson <pelson.pub@gmail.com>
2016-07-04 12:12:49 +0100

    Fix the failing build.

--- make.sh
+++ make.sh
@@ -16,20 +16,19 @@ cd build
 cp -rf ../course_content/images images
 cp -rf ../course_content/resources resources
 
-cd html
 for name in "numpy_intro" "matplotlib_intro" "cartopy_intro" "iris_intro"
 do
     #ipython nbconvert --to slides ../../course_content/${name}.ipynb
     # Build static (html) copies of the course content.
-    jupyter nbconvert --to html ../../course_content/notebooks/${name}.ipynb
+    jupyter nbconvert --to html ../course_content/notebooks/${name}.ipynb
+    mv ../course_content/notebooks/*.html ./html/
     # Make IPython notebooks of the course content with cell output cleared.
-    python ../../utils/nbutil.py ../../course_content/notebooks/${name}.ipynb ../notebooks/${name}.ipynb --clear-output
+    python ../utils/nbutil.py ../course_content/notebooks/${name}.ipynb ./notebooks/${name}.ipynb --clear-output
 done
 
-cd ../solutions
 for name in "numpy_exercise_1.ipynb" "numpy_exercise_2.ipynb" "matplotlib_exercise_2.ipynb" "matplotlib_exercise_3.ipynb" "cartopy_exercise_1.ipynb" "iris_exercise_1.ipynb" "iris_exercise_2.ipynb" "iris_exercise_3.ipynb" "iris_exercise_4.ipynb" "iris_exercise_5.ipynb" "iris_exercise_6.ipynb" "iris_exercise_7.ipynb"
 do
-    python ../../utils/nbutil.py ../../course_content/solutions/${name} ${name}
+    python ../utils/nbutil.py ../course_content/solutions/${name} ./solutions/${name}
 done
 
 
