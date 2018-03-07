699b8c8d8c0d52240f96157b3fc7636c7435cba0
Bill Little <bill.james.little@gmail.com>
2018-03-07 11:22:10 +0000

    tidy and automate static build

--- make.sh
+++ make.sh
@@ -3,6 +3,8 @@
 # Fail on first exception.
 set -e
 
+PROG=$(basename ${0})
+
 # Clean out the build folder then replace the files we need to keep.
 [ -d "./build" ] && rm -rf build/* || mkdir build
 cp .gitignore LICENSE README.md build
@@ -12,21 +14,37 @@ mkdir -p build/html
 mkdir -p build/notebooks
 mkdir -p build/solutions
 
+# Change to the local build directory.
 cd build
-cp -rf ../course_content/images images
-cp -rf ../course_content/resources resources
 
-for name in "numpy_intro" "matplotlib_intro" "cartopy_intro" "iris_intro" "dask_intro"
+# All directories are relative to the build directory.
+CONTENT_DIR="../course_content"
+IMAGES_DIR="${CONTENT_DIR}/images"
+RESOURCES_DIR="${CONTENT_DIR}/resources"
+NOTEBOOKS_DIR="${CONTENT_DIR}/notebooks"
+
+# Check required source course content directories exist.
+for DIR in "${IMAGES_DIR}" "${RESOURCES_DIR}" "${NOTEBOOKS_DIR}"
 do
-    #ipython nbconvert --to slides ../../course_content/${name}.ipynb
-    # Build static (html) copies of the course content.
-    jupyter nbconvert --to html ../course_content/notebooks/${name}.ipynb
-    mv ../course_content/notebooks/*.html ./html/
+    [ ! -d "${DIR}" ] && echo "${PROG}: Missing directory, \"${DIR}\"" && exit 1
 done
 
-#.reveal aside.notes {
-#   visibility: inline;
-#}
+# Copy required resources to the local build directory.
+cp -rf ${IMAGES_DIR} images
+cp -rf ${RESOURCES_DIR} resources
+
+# Enumerate the course content notebooks.
+NOTEBOOKS=$(ls -1 ${NOTEBOOKS_DIR}/*.ipynb)
+
+# Build static (HTML) copies of the course content.
+for NOTEBOOK in ${NOTEBOOKS}
+do
+    jupyter nbconvert --to html ${NOTEBOOK}
+    echo
+done
+
+# Move built static (HTML) content to the local build directory.
+mv ${NOTEBOOKS_DIR}/*.html html
 
 
 
