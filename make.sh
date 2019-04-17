b5591af335402eba338cf10f571cbd24afa7c0d7
lbdreyer <laura.dreyer@metoffice.gov.uk>
2019-04-17 14:35:03 +0100

    fix make.sh

--- make.sh
+++ make.sh
@@ -3,26 +3,6 @@
 # Fail on first exception.
 set -e
 
-# Clean out the build folder then replace the files we need to keep.
-mkdir -p build
-rm -rf build/*
-cp .gitignore LICENSE README.md build
-
-
-cd build
-
-# Set up the folders we will need.
-mkdir -p html
-for dirname in extra_courses cartopy_course iris_course
-    do
-        mkdir -p extra
-    done
-
-cp -Rf ../course_content/extra_courses/images extra_courses/images
-cp -Rf ../course_content/iris_course/images iris_course/images
-cp -Rf ../course_content/iris_course/resources iris_course/resources
-
-
 process_notebooks () {
     notebook_file=$1
     dirname=$2
@@ -34,13 +14,11 @@ process_notebooks () {
         --ExecutePreprocessor.kernel_name=python3 \
         --ExecutePreprocessor.timeout=180 \
         --execute --allow-errors \
-        --output-dir $dirname \
-        --output $notebook_file
 }
 
 
 for dirname in extra_courses cartopy_course iris_course
-    for notebook_file in $(ls ../course_content/$dirname/*.ipynb)
+    for notebook_file in $(ls course_content/$dirname/*.ipynb)
         do
             process_notebooks $dirname $notebook_file
         done
